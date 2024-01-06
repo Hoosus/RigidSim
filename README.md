45457e0edd5f63ff91b3ac17346dfe0be0d40842
Hoosus <823546256@qq.com>
2024-01-07 00:50:40 +0800

    Add reading obj

--- README.md
+++ README.md
@@ -8,17 +8,17 @@ Clone the repo
 git clone --recursive git@github.com:Hoosus/RigidSim.git
 ```
 
-Download Optix 7.6 (7.7, 8.0 or higher not supported due to LuisaCompute), and move all the header files in place.
-See `src/compute/src/backends/cuda/optix/README.md`
+~~Download Optix 7.6, and move all the header files in place.~~
+~~See `src/compute/src/backends/cuda/optix/README.md`~~
 
 ```
 mkdir build
 cd build
-cmake -DCMAKE_USE_OPENSSL=ON .. -D OptiX_DIR=~/NVIDIA-OptiX-SDK-7.6.0-linux64-x86_64
+cmake -DCMAKE_USE_OPENSSL=ON .. -D OptiX_DIR=/home/xuejun/NVIDIA-OptiX-SDK-7.6.0-linux64-x86_64 -D NVTT_DIR=/home/xuejun/nvtt
 ```
 
 ## Run
 
 ```
-./bin/app -b cuda -s 1600x1200 -t 1 --dump "./1.jpg" 
+./bin/app -b cuda -s 1600x1200 -t 1 -n 10 --fps 1 --dump "../1.mp4" 
 ```
--- src/app/app.cpp
+++ src/app/app.cpp
@@ -10,17 +10,53 @@ using namespace luisa;
 using namespace luisa::compute;
 using namespace rigid_sim;
 
+void ReadObj(std::string inputfile) {
+  tinyobj::ObjReaderConfig reader_config;
+  tinyobj::ObjReader reader;
 
+  if (!reader.ParseFromFile(inputfile, reader_config)) {
+    if (!reader.Error().empty()) {
+      std::cerr << "TinyObjReader: " << reader.Error();
+    }
+    exit(1);
+  }
+
+  if (!reader.Warning().empty()) {
+    std::cout << "TinyObjReader: " << reader.Warning();
+  }
+
+  auto& attrib = reader.GetAttrib();
+  auto& shapes = reader.GetShapes();
+  // auto& materials = reader.GetMaterials(); 
+
+  assert(shapes.size() == 1);
+
+  int num_verts = attrib.vertices.size();
+  int num_faces = shapes[0].mesh.num_face_vertices.size();
+
+  std::vector<float> verts = attrib.vertices;
+  std::vector<float> faces;
+  faces.resize(num_faces * 3);
+  for (int i = 0; i < num_faces; ++i) {
+    faces[3 * i] = shapes[0].mesh.indices[i].vertex_index * 3;
+    faces[3 * i + 1] = shapes[0].mesh.indices[i].vertex_index * 3 + 1;
+    faces[3 * i + 2] = shapes[0].mesh.indices[i].vertex_index * 3 + 2;
+  }
+
+  // TODO
+}
 
 int main(int argc, char *argv[]) {
 
-    gui::ShaderToy toy{argc, argv};
+  gui::ShaderToy toy{argc, argv};
 
-    auto& stream = toy.stream();
+  std::string inputfile = "../assets/box.obj";
+  ReadObj(inputfile);
 
-    Scene scene{};
+  auto& stream = toy.stream();
+  Scene scene{};
 
-    toy.run([&](Float2 fragCoord, Float2 iResolution, Float time, Float4 cursor) noexcept {
-        return make_float3(make_float2(fragCoord.xy() / 1000.f), 1.f);
-    });
+  toy.run([&](Float2 fragCoord, Float2 iResolution, Float time, Float4 cursor) noexcept {
+    return make_float3(make_float2(fragCoord.xy() / 1000.f), 1.f);
+  });
 }
